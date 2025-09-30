f7eb4305e80a9930df288e46e22ea98f989e9951
pranav-8660 <pranavcse2003@gmail.com>
2025-09-30 17:57:03 +0530

    assignment-1 starter

---
+++ assignment-1-springboot/src/main/java/com/pranavv51/assignment_1_springboot/Assignment1SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.pranavv51.assignment_1_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment1SpringbootApplication.class, args);
+	}
+
+}
---
+++ assignment-1-springboot/src/test/java/com/pranavv51/assignment_1_springboot/Assignment1SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranavv51.assignment_1_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
