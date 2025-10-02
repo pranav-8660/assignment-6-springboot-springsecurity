2ac6a6f6ae9827949d0e66855e4361be69108415
pranav-8660 <pranavcse2003@gmail.com>
2025-10-02 17:13:38 +0530

    starter for asssignment-2

---
+++ assignment-2-developer-task-tracker/src/main/java/com/pranavv51/assignment_2_developer_task_tracker/Assignment2DeveloperTaskTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pranavv51.assignment_2_developer_task_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment2DeveloperTaskTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment2DeveloperTaskTrackerApplication.class, args);
+	}
+
+}
---
+++ assignment-2-developer-task-tracker/src/test/java/com/pranavv51/assignment_2_developer_task_tracker/Assignment2DeveloperTaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranavv51.assignment_2_developer_task_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment2DeveloperTaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
